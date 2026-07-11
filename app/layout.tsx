4dff2a0ecf38c1e2a6f46c8ef9ce88a37740e915
devalto <51037255+dev-alto@users.noreply.github.com>
2026-07-10 22:35:15 -0700

    feat: add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
+import { Analytics } from '@vercel/analytics/next'
+
 import './globals.css'
 
 const inter = Inter({
@@ -22,6 +25,7 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} h-full antialiased`}
     >
+      <Analytics />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   )
